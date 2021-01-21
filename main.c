07846421b79990bf0a26f1e7242a3514384c7c70
loubnas <loubnasoussi15489@gmail.com>
2021-01-21 14:34:35 +0100

    Modific

--- main.c
+++ main.c
@@ -8,12 +8,37 @@
 // function convert int to binary 
 // return binary 
 
-int functBinary(int a, int b){
+
+
+
+int *functBinary(int a){
+	int i = 0,j=0,b[4],bin;
+	while(a !=0){
+		b[j]=a%2;
+		bin=a*i;
+		a=a/2;
+		i=i*10;
+		j++;
+		
+	}
+return b;
+	
+	 
 }
 
 
 
 
 int main() {
+	int i=4;
+	//int a[]={1,0,0,0,1};
+	int *p;
+	p = functBinary(16);
+	
+	while(i >= 0)
+	printf("%d",p[i--]);
+	
+	
+	
 	return 0;
 }
