4a532f23f8531ca4b63c3a1ed0efbe53e49b60ae
Yassine-yb <yassine.yb.99@gmail.com>
2021-01-21 11:39:04 +0100

    Voici la fonction somme

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
 //Somme des deux valeur binaire
-int Sum(){
+int Sum(int *a, int *b, int *sortie){
 	
-	
-	
-	
+
+    int i;
+ 	
+ 	
+ 	//Application du ET logique
+    for(i=0; i<8; i++)
+    {
+
+        if((a[i] == '0') && (b[i] == '0'))
+            sortie[i] = '0';
+        if((a[i] == '0') && (b[i] == '1'))
+            sortie[i] = '1';
+        if((a[i] == '1') && (b[i] == '0'))
+            sortie[i] = '1';
+        if((a[i] == '1') && (b[i] == '1'))
+            sortie[i] = '0';
+    
+    }
+    
+    //Affichage
+    for(i=0; i<8; i++)
+    	printf ("La somme de ces 2 valeurs est:   %d", sortie[i]);
 }
+	
+	
 
 int main(){
 	
