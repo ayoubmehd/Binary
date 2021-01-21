73587bd11f8c4eb0fc5838a4b432b3863969abf9
ayoubmehd <safi-_-gio-raja@live.fr>
2021-01-21 14:25:41 +0100

    Add binary to int

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /*
  * return int integer 10 base
@@ -6,10 +8,43 @@
  *
  **/
 
-int binaryToInt() {
+int binaryToInt(int binary[], int size) {
+	
+	int i, j = size - 1, sum = 0;
+	
+	for (i = 0; i < size; i++) {
+		
+//		printf("%d ) val: %d - res : %d - pow : %d\n", j, binary[i], binary[i] * ((int)pow(2, j)), ((int)pow(2, j)));
+		
+
+		sum += binary[i] * ((int)pow(2, j));
+		j--;
+	}
+	
+	printf("sum : %d, size : %d\n", sum, size);
+	
+	return sum;
+}
+
+// function convert int to binary 
+// return binary 
+
+int functBinary(int a, int b) {
+	
+}
+
+//Somme des deux valeur binaire
+int Sum(){
+	
 	
 }
 
 int main() {
+	int size = 4;
+	
+	int bin[] = { 1, 1, 1, 0};
+	
+	printf("%d",binaryToInt(bin, size));
+		
 	return 0;
 }
